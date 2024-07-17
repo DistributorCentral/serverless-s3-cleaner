9ac67ef83a7c241e42832bc9161a2d529bbb839c
Chris Schlemmer <schris@schris.net>
2024-07-16 20:12:21 -0500

    Upgrade to @aws-sdk v3 client

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,63 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
+import {
+  S3Client,
+  HeadBucketCommand,
+  DeleteObjectsCommand,
+  ListObjectVersionsCommand,
+  ObjectIdentifier,
+} from '@aws-sdk/client-s3';
 import type { JSONSchemaType } from 'ajv';
-import type {
-  DeleteObjectsOutput,
-  DeleteObjectsRequest,
-  HeadBucketRequest,
-  ListObjectVersionsOutput, ListObjectVersionsRequest,
-  ObjectIdentifierList
-} from 'aws-sdk/clients/s3';
 import prompt from 'prompt';
 import Serverless from 'serverless';
 import Plugin, { Logging } from 'serverless/classes/Plugin';
 import Aws from 'serverless/plugins/aws/provider/awsProvider';
+
+type ObjectIdentifierList = ObjectIdentifier[];
 
 export default class ServerlessS3Cleaner implements Plugin {
   public commands: Plugin.Commands;
   public hooks: Plugin.Hooks;
   public provider: Aws;
   private log: Logging['log'];
+  private s3Client: S3Client;
 
   private configSchema: JSONSchemaType<ServerlessS3CleanerConfig> = {
     type: 'object',
     properties: {
       prompt: { type: 'boolean', nullable: true, default: false },
       buckets: {
-        type: 'array', uniqueItems: true, items: { type: 'string' }, nullable: true
+        type: 'array',
+        uniqueItems: true,
+        items: { type: 'string' },
+        nullable: true,
       },
       bucketsToCleanOnDeploy: {
-        type: 'array', uniqueItems: true, items: { type: 'string' }, nullable: true
+        type: 'array',
+        uniqueItems: true,
+        items: { type: 'string' },
+        nullable: true,
       },
     },
     additionalProperties: false,
-    anyOf: [
-      { required: ['buckets'] },
-      { required: ['bucketsToCleanOnDeploy'] }
-    ]
+    anyOf: [{ required: ['buckets'] }, { required: ['bucketsToCleanOnDeploy'] }],
   };
 
   constructor(private readonly serverless: Serverless, _options, logging: Logging) {
-
     this.provider = this.serverless.getProvider('aws');
     this.log = logging.log;
+    this.s3Client = new S3Client({ region: this.provider.getRegion() });
     this.serverless.configSchemaHandler.defineCustomProperties({
       type: 'object',
       properties: {
-        'serverless-s3-cleaner': this.configSchema
-      }
+        'serverless-s3-cleaner': this.configSchema,
+      },
     });
 
     this.commands = {
       s3remove: {
         usage: 'Remove all files in S3 buckets',
-        lifecycleEvents: [
-          'remove'
-        ]
-      }
+        lifecycleEvents: ['remove'],
+      },
     };
 
     this.hooks = {
@@ -69,13 +73,15 @@ export default class ServerlessS3Cleaner implements Plugin {
 
     if (config.prompt) {
       prompt.start();
-      const bucketPromptResults = await prompt.get(bucketsToEmpty.map(bucket => ({
-        name: bucket,
-        description: `Empty bucket ${bucket}. Are you sure? [yes/no]:`,
-        pattern: /(yes|no)/,
-        default: 'yes',
-        message: 'Must respond yes or no',
-      })));
+      const bucketPromptResults = await prompt.get(
+        bucketsToEmpty.map((bucket) => ({
+          name: bucket,
+          description: `Empty bucket ${bucket}. Are you sure? [yes/no]:`,
+          pattern: /(yes|no)/,
+          default: 'yes',
+          message: 'Must respond yes or no',
+        })),
+      );
 
       bucketsToEmpty = [];
       for (const bucket of Object.keys(bucketPromptResults)) {
@@ -88,8 +94,6 @@ export default class ServerlessS3Cleaner implements Plugin {
       }
     }
 
-    // Filter out inaccessible buckets before doing the work;
-    // this is so we don't log unnecessary/ugly errors in case e.g. old buckets left in bucketsToCleanOnDeploy have already been removed
     const existingBuckets: string[] = [];
     for (const bucket of bucketsToEmpty) {
       const exists = await this.bucketExists(bucket);
@@ -100,42 +104,44 @@ export default class ServerlessS3Cleaner implements Plugin {
       }
     }
 
-    // Parallelize the removal to speed things up
-    const removePromises = existingBuckets.map(bucket => this
-      .listBucketKeys(bucket)
-      .then(keys => this.deleteObjects(bucket, keys))
-      .then(() => this.log.success(`${bucket} successfully emptied`))
-      .catch(err => this.log.error(`${bucket} cannot be emptied. ${err}`)));
+    const removePromises = existingBuckets.map((bucket) =>
+      this.listBucketKeys(bucket)
+        .then((keys) => this.deleteObjects(bucket, keys))
+        .then(() => this.log.success(`${bucket} successfully emptied`))
+        .catch((err) => this.log.error(`${bucket} cannot be emptied. ${err}`)),
+    );
 
     await Promise.all(removePromises);
   }
 
   private async bucketExists(bucket: string): Promise<boolean> {
-    const params: HeadBucketRequest = { Bucket: bucket };
-    return this.provider.request('S3', 'headBucket', params)
-      .then(() => true)
-      .catch(() => false);
+    const params = { Bucket: bucket };
+    try {
+      await this.s3Client.send(new HeadBucketCommand(params));
+      return true;
+    } catch {
+      return false;
+    }
   }
 
   private async deleteObjects(bucket: string, keys: ObjectIdentifierList): Promise<void> {
     const maxDeleteKeys = 1000;
-    const params: DeleteObjectsRequest[] = [];
+    const params: DeleteObjectsCommand[] = [];
     for (let i = 0; i < keys.length; i += maxDeleteKeys) {
-      params.push({
-        Bucket: bucket,
-        Delete: {
-          Objects: keys.slice(i, i + maxDeleteKeys),
-          Quiet: true
-        }
-      });
+      params.push(
+        new DeleteObjectsCommand({
+          Bucket: bucket,
+          Delete: {
+            Objects: keys.slice(i, i + maxDeleteKeys),
+            Quiet: true,
+          },
+        }),
+      );
     }
 
-    const deleteResults: DeleteObjectsOutput[] = await Promise.all(
-      params.map(param => this.provider.request('S3', 'deleteObjects', param))
-    );
+    const deleteResults = await Promise.all(params.map((param) => this.s3Client.send(param)));
 
-    // to avoid dumping too many details in the output, we choose to list the first error message available, if any
-    const firstErrorResult = deleteResults.find(dr => dr.Errors && dr.Errors.length > 0);
+    const firstErrorResult = deleteResults.find((dr) => dr.Errors && dr.Errors.length > 0);
     if (firstErrorResult) {
       const errInfo = firstErrorResult.Errors![0];
       throw new Error(`${errInfo.Key} - ${errInfo.Message}`);
@@ -143,18 +149,20 @@ export default class ServerlessS3Cleaner implements Plugin {
   }
 
   private async listBucketKeys(bucketName: string): Promise<ObjectIdentifierList> {
-    const listParams: ListObjectVersionsRequest = {
-      Bucket: bucketName
-    };
+    const listParams = { Bucket: bucketName };
     let bucketKeys: ObjectIdentifierList = [];
 
     while (true) {
-      const listResult: ListObjectVersionsOutput = await this.provider.request('S3', 'listObjectVersions', listParams);
+      const listResult = await this.s3Client.send(new ListObjectVersionsCommand(listParams));
       if (listResult.Versions) {
-        bucketKeys = bucketKeys.concat(listResult.Versions.map(item => ({ Key: item.Key!, VersionId: item.VersionId! })));
+        bucketKeys = bucketKeys.concat(
+          listResult.Versions.map((item) => ({ Key: item.Key!, VersionId: item.VersionId! })),
+        );
       }
       if (listResult.DeleteMarkers) {
-        bucketKeys = bucketKeys.concat(listResult.DeleteMarkers.map(item => ({ Key: item.Key!, VersionId: item.VersionId! })));
+        bucketKeys = bucketKeys.concat(
+          listResult.DeleteMarkers.map((item) => ({ Key: item.Key!, VersionId: item.VersionId! })),
+        );
       }
 
       if (!listResult.IsTruncated) {
@@ -170,7 +178,9 @@ export default class ServerlessS3Cleaner implements Plugin {
   private loadConfig(): Required<ServerlessS3CleanerConfig> {
     const providedConfig: Partial<ServerlessS3CleanerConfig> = this.serverless.service.custom['serverless-s3-cleaner'];
     if (!providedConfig.buckets && !providedConfig.bucketsToCleanOnDeploy) {
-      throw new Error('You must configure "buckets" or "bucketsToCleanOnDeploy" parameters in custom > serverless-s3-cleaner section');
+      throw new Error(
+        'You must configure "buckets" or "bucketsToCleanOnDeploy" parameters in custom > serverless-s3-cleaner section',
+      );
     }
 
     return {
